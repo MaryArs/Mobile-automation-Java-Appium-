5a44237557f5f8f227e2ae789e23310972b00651
MarinaA <mary.arsitova@gmail.com>
2018-08-15 16:13:27 -0700

    Find text in search field.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,7 +3,11 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -29,6 +33,19 @@ public class FirstTest {
     }
     @Test
     public void firstTest(){
-        System.out.println("First test run!");
+       // WebElement elememt_to_init_search = driver.findElementByXPath("//*[contains(@text,'Search Wikipedia')]");
+       // elememt_to_init_search.click();
+        WebElement element_to_enter_search_line = waitForElementPresentByXPath("//*[contains(@text,'Search…')]",
+                                                                                      "Cannot find search input", 5);
+                //driver.findElementByXPath("//*[contains(@text,'Search…')]");
+        element_to_enter_search_line.sendKeys("Appium");
+        //System.out.println("First test run!");
+    }
+
+    private WebElement waitForElementPresentByXPath(String xpath, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return  wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 }
