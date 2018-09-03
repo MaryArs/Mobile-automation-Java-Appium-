8d82097cf25f3a26c240f7e068b3a6d748ce0b73
MarinaA <mary.arsitova@gmail.com>
2018-09-03 14:43:56 -0700

    Save first article. Swipe it left and delete.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -81,7 +81,7 @@ public class FirstTest {
     }
 
     @Test
-    public  void tectCompareArticleTitle(){
+    public void tectCompareArticleTitle() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find Searc Wikipedia input",
@@ -103,12 +103,12 @@ public class FirstTest {
         Assert.assertEquals(
                 "We see unexpected title",
                 "Java (programming language)",
-                 article_title);
+                article_title);
 
     }
 
     @Test
-    public  void tectSwipeArticle(){
+    public void testSwipeArticle() {
         waitForElementAndClick(
                 By.xpath("//*[contains(@text,'Search Wikipedia')]"),
                 "Cannot find Searc Wikipedia input",
@@ -122,13 +122,102 @@ public class FirstTest {
                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_title'][@text='Appium']"),
                 "Appium in search",
                 5);
-       waitForElementPresent(
-               By.id("org.wikipedia:id/view_page_title_text"),
-               "Cannot find article title",
-               15);
-       swipeUpToFindElement(By.xpath("//*[@text = 'View page in browser']"),
-               "Cannot find the end of the article.",
-               20);
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15);
+        swipeUpToFindElement(By.xpath("//*[@text = 'View page in browser']"),
+                "Cannot find the end of the article.",
+                20);
+
+
+    }
+
+    @Test
+    public void saveFirstArticleToMyList() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text,'Search Wikipedia')]"),
+                "Cannot find Searc Wikipedia input",
+                5);
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                "Java",
+                "Cannot find search input",
+                5);
+        waitForElementAndClick(
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']"),
+                "Cannot find Wikipedia input",
+                5);
+        waitForElementPresent(
+                By.id("org.wikipedia:id/view_page_title_text"),
+                "Cannot find article title",
+                15);
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageView[@content-desc='More options']"),
+                "Cannot find button to open article options",
+                30);
+
+        //Click on the button Add to reading list
+        waitForElementAndClick(
+                By.xpath("//*[@text='Add to reading list']"),
+                "Cannot find option to add article to reading list",
+                5);
+
+        //Click on the button Got it.
+        waitForElementAndClick(
+                By.id("org.wikipedia:id/onboarding_button"),
+                "Cannot find 'Got it' tip overlay",
+                5);
+
+        //Clear the title field.
+        waitForElementAndClear(
+                By.id("org.wikipedia:id/text_input"),
+                "Cannot find input to set name of articles folder",
+                5);
+
+        //Enter title for articles folder.
+        String name_of_folder ="Learning programming";
+        waitForElementAndSendKeys(
+                By.id("org.wikipedia:id/text_input"),
+                name_of_folder,
+                "Cannot put text into articles folder input",
+                5);
+
+        //Click on the button OK.
+        waitForElementAndClick(
+                By.xpath("//*[@text='OK']"),
+                "Cannot press OK button",
+                5);
+
+        //Click on the X button in the left corner.
+        waitForElementAndClick(
+                By.xpath("//android.widget.ImageButton[@content-desc='Navigate up']"),
+                "Cannot close article, cannot find X link.",
+                5);
+
+        //Click on the navigation button to My list.
+        waitForElementAndClick(
+                By.xpath("//android.widget.FrameLayout[@content-desc='My lists']"),
+                "Cannot find navigation button to My list.",
+                5);
+
+        //Click on the navigation button to My list.
+        waitForElementAndClick(
+                By.xpath("//*[@text='" + name_of_folder +"']"),
+                "Cannot find created folder.",
+                5);
+
+        //Swipe article from the right to the left and delete the article.
+        swipeElementToLeft(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot find saved article");
+
+        //Check that the article was deleted.
+        waitForElementNotPresent(
+                By.xpath("//*[@text='Java (programming language)']"),
+                "Cannot delete saved article.",
+                5);
+
 
 
     }
@@ -162,41 +251,67 @@ public class FirstTest {
         return wait.until(ExpectedConditions.invisibilityOfElementLocated(by));
     }
 
-    private  WebElement waitForElementAndClear(By by, String error_massage, long timeoutInSeconds){
+    private WebElement waitForElementAndClear(By by, String error_massage, long timeoutInSeconds) {
         WebElement element = waitForElementPresent(by, error_massage, timeoutInSeconds);
         element.clear();
-        return  element;
+        return element;
     }
 
     //Swipe up from the footer to the header.
-    protected void swipeUp(int timeOfSwipe){
+    protected void swipeUp(int timeOfSwipe) {
         TouchAction action = new TouchAction(driver);
         Dimension size = driver.manage().window().getSize();
         System.out.println(size);
-        int x = size.width/2;
-        int start_y =(int) (size.height*0.8);
-        int end_y = (int) (size.height*0.2);
-        action.press(x, start_y).waitAction(timeOfSwipe).moveTo(x, end_y).release().perform();
-
+        int x = size.width / 2;
+        int start_y = (int) (size.height * 0.8);
+        int end_y = (int) (size.height * 0.2);
+        action
+                .press(x, start_y)
+                .waitAction(timeOfSwipe)
+                .moveTo(x, end_y)
+                .release()
+                .perform();
     }
 
-    protected void swipeUpQuick(){
+    protected void swipeUpQuick() {
         swipeUp(200);
     }
 
     //Swipe while element by is present on the page.
     //max_swipes - max amount of swipes we need.
     //already_swiped - the amount of real swipes
-    protected void swipeUpToFindElement(By by, String error_message, int max_swipes){
+    protected void swipeUpToFindElement(By by, String error_message, int max_swipes) {
         int already_swiped = 0;
-        while (driver.findElements(by).size()==0){
-            if(already_swiped > max_swipes){
+        while (driver.findElements(by).size() == 0) {
+            if (already_swiped > max_swipes) {
                 waitForElementPresent(by, "Cannot find element by swiping up.\n" + error_message, 0);
                 return;
             }
             swipeUpQuick();
             ++already_swiped;
         }
+    }
+
+    protected void swipeElementToLeft(By by, String error_message) {
+       WebElement element = waitForElementPresent(
+               by,
+               error_message,
+               10);
+       int left_x = element.getLocation().getX();
+       int right_x = left_x + element.getSize().getWidth();
+       int upper_y = element.getLocation().getY();
+       int lower_y = upper_y + element.getSize().getHeight();
+       int middle_y = (upper_y + lower_y)/2;
+
+       TouchAction action = new TouchAction(driver);
+       action
+               .press(right_x, middle_y)
+               .waitAction(300)
+               .moveTo(left_x, middle_y)
+               .release()
+               .perform();
+
+
 
     }
 }
